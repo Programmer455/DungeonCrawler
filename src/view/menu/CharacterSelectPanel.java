c776c8acfbe3830ae48a3a51ac0cb2d4b4e96103
neals725 <sgn0005@uah.edu>
2022-11-29 11:30:36 -0600

    Added CharacterSelectPanel

---
+++ src/view/menu/CharacterSelectPanel.java
@@ -0,0 +1,137 @@
+package view.menu;
+
+import model.PlayerCharacter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author Sawyer Neal
+ */
+public class CharacterSelectPanel extends JPanel implements ActionListener {
+
+    private final MenuFrame parentFrame;
+    
+    private final GridBagConstraints gbc;
+    
+    // Components
+    private final JLabel descLabel = new JLabel("Create Your Character!");
+    private final JLabel nameLabel = new JLabel("Enter Name");
+    private final JTextField nameTextField = new JTextField();
+    private final JLabel archetypeLabel = new JLabel("Pick a Class");
+    private final JRadioButton warriorButton = new JRadioButton("Warrior");
+    private final JRadioButton mageButton = new JRadioButton("Mage");
+    private final JRadioButton paladinButton = new JRadioButton("Paladin");
+    private final JButton enterButton = new JButton("Begin");
+    private final JButton backButton = new JButton("<- Go Back");
+    private final ButtonGroup classes = new ButtonGroup();
+    private final Dimension maxTextFieldDim = new Dimension(600, 30);
+    
+    
+    
+    public CharacterSelectPanel(MenuFrame parentFrame) {
+        
+        this.parentFrame = parentFrame;
+        this.setLayout(new GridBagLayout());
+        gbc = new GridBagConstraints();
+        
+        classes.add(warriorButton);
+        classes.add(mageButton);
+        classes.add(paladinButton);
+        
+        descLabel.setFont(new Font("Verdana", Font.BOLD, 30));
+        nameLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+        archetypeLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+        
+        nameTextField.setMaximumSize(maxTextFieldDim);
+        
+        backButton.setFont(new Font("Verdana", Font.BOLD, 10));
+        backButton.setBackground(Color.RED);
+        backButton.setForeground(Color.WHITE);
+        
+        enterButton.setFont(new Font("Verdana", Font.BOLD, 20));
+        enterButton.setBackground(Color.RED);
+        enterButton.setForeground(Color.WHITE);
+        
+        warriorButton.setFont(new Font("Verdana", Font.BOLD, 10));
+        mageButton.setFont(new Font("Verdana", Font.BOLD, 10));
+        paladinButton.setFont(new Font("Verdana", Font.BOLD, 10));
+        
+        addElements();
+        addActionEvents();
+    }
+    
+    private void addElements() {
+        
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        this.add(backButton, gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.ipady = 50;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(descLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.ipady = 10;
+        
+        this.add(nameLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(nameTextField, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        this.add(archetypeLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        this.add(warriorButton, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        this.add(mageButton, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        this.add(paladinButton, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 10;
+        gbc.gridwidth = 4;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(enterButton, gbc);
+    }
+    
+    private void addActionEvents() {
+        warriorButton.addActionListener(this);
+        mageButton.addActionListener(this);
+        paladinButton.addActionListener(this);
+        enterButton.addActionListener(this);
+        backButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == enterButton) {
+            /**
+            if (warriorButton.isSelected()) {
+                PlayerCharacter PC = new PlayerCharacter(nameTextField.getText(), "Warrior");
+                
+            }
+            */
+        }
+        else if (e.getSource() == backButton) {
+            parentFrame.openTitle();
+        }
+    }
+}
