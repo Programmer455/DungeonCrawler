c71148c2bb7a314d4c21a03abf8a5d47c92cfb70
neals725 <sgn0005@uah.edu>
2022-11-29 11:31:19 -0600

    Moved to no package

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import view.menu.*;
+
+/**
+ *
+ * @author Team 02
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       MenuFrame frame = new MenuFrame();
+       frame.setVisible(true);
+       
+       
+       /**
+       Dungeon test = new Dungeon(3, 3, 1);
+       test.setEmptyDungeon();
+       test.setLevel();
+       test.readLevel();
+       new Dungeon_UI(test);
+       */
+       
+    }
+    
+}
